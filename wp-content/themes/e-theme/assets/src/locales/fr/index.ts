8056cde0491c8b5b99ebc5efb12bfbbd2f92edae
Expipoint admin <admingit@expipoint.com>
2025-11-25 15:21:15 +0100

    additional lang

---
+++ wp-content/themes/e-theme/assets/src/locales/fr/index.ts
@@ -0,0 +1,54 @@
+import competitiveAdvantageHero from "./competitive-advantage-hero.json";
+import security from "./security.json";
+import stats from "./stats.json";
+import leadCapture from "./lead-capture.json";
+import solutionsOverview from "./solutions-overview.json";
+import problemAndSolution from "./problem-and-solution.json";
+import knowledge from "./knowledge.json";
+import positioning from "./positioning.json";
+import hero from "./hero.json";
+import autoVSteamWork from "./autoVSteamWork.json";
+import makeImpact from "./makeImpact.json";
+import articles from "./articles.json";
+
+export default {
+  ...competitiveAdvantageHero,
+  ...security,
+  ...stats,
+  ...leadCapture,
+  ...solutionsOverview,
+  ...problemAndSolution,
+  ...knowledge,
+  ...positioning,
+  ...hero,
+  ...autoVSteamWork,
+  ...makeImpact,
+  ...articles,
+  consultant: {
+    title: "Was wäre, wenn Ihr bester Berater nicht menschlich wäre?",
+    button: "Anfrage senden",
+  },
+  footer: {
+    description:
+      "AICONIQ entwickelt AI Lösungen für den Mittelstand - um die Wettbewerbsfähigkeit unseres Industriestandortes zu erhalten.",
+    social: "Social",
+    links: "Links",
+    footerLinks: {
+      impressum: "Impressum",
+      terms: "Nutzungsbedingungen",
+      // status: "Systemstatus",
+    },
+  },
+  navigation: {
+    home: "Home",
+    solutions: "Lösungen",
+    about: "Über AICONIQ",
+    contact: "Kontakt",
+  },
+  chatButton: {
+    label: "Sprich mit mir",
+  },
+  imprint: {
+    title: "Impressum",
+  },
+};
